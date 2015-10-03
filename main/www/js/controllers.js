5730794fc775a4fe0bd43d64c1c37bb46067cdd8
Lashit Jain <jainlashit@gmail.com>
2015-10-04 02:22:31 +0530

    Map Code Added

--- main/www/js/controllers.js
+++ main/www/js/controllers.js
@@ -21,8 +21,33 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('MapCtrl', function() {
-  console.log("Reporting from map controller");
+.controller('MapCtrl', function($scope, $ionicLoading) {
+  $scope.mapCreated = function(map) {
+    $scope.map = map;
+  };
+
+  $scope.centerOnMe = function () {
+    console.log("Centering");
+    if (!$scope.map) {
+      console.log("Error");
+      return;
+    }
+
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log('Got pos', pos);
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.latitude = pos.coords.latitude;
+      $scope.longitude = pos.coords.longitude;
+      $scope.loading.hide();
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
 })
 
 .controller('CameraCtrl', function() {
